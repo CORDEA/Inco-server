1236e07f2a4cc3f19ceeb5465d78ed1d029d82bf
Yoshihiro Tanaka <contact@cordea.jp>
2017-10-10 19:08:58 +0900

    specify json key

--- history.go
+++ history.go
@@ -5,7 +5,7 @@ import (
 )
 
 type History struct {
-	ID int64 `gorm:"primary_key"`
-	Url string `gorm:"not null"`
-	CreatedAt time.Time `gorm:"not null"`
+	ID int64 `gorm:"primary_key" json:"id"`
+	Url string `gorm:"not null" json:"url"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at"`
 }
